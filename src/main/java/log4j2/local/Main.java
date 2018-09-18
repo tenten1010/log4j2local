fcb08516440651de13f33a8df71947039852b01b
MENG TIANYI <JP23417@w022571807502m.local>
2018-09-18 20:23:29 +0900

    0918

--- src/main/java/log4j2/local/Main.java
+++ src/main/java/log4j2/local/Main.java
@@ -1,13 +1,15 @@
 package log4j2.local;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 
 
 public class Main {
     private static Logger logger = LogManager.getLogger("test");
 
     public static void main(String[] args) {
-//        logger.info("info");
-        logger.error("error");
+        int i = 0;
+        ThreadContext.put("appId", "110");
+        logger.error("%d: エラーメッセージです", i);
     }
 }
