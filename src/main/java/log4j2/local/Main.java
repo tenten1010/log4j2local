10a75b78cbf3e3238d69695a503dd025a68bd2a7
MENG TIANYI <JP23417@w022571807502m.local>
2018-09-19 16:17:28 +0900

    done

--- src/main/java/log4j2/local/Main.java
+++ src/main/java/log4j2/local/Main.java
@@ -2,14 +2,15 @@ package log4j2.local;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
-
+import java.util.UUID;
 
 public class Main {
-    private static Logger logger = LogManager.getLogger("test");
+    private static Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         int i = 0;
         ThreadContext.put("appId", "110");
-        logger.error("%d: エラーメッセージです", i);
+        ThreadContext.put("id", UUID.randomUUID().toString().replace("-", "").toLowerCase());
+        logger.error("エラーメッセージです");
     }
 }
